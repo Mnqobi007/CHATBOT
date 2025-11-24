9b4598a070cb32078353e14b9d4a79e2549160ac
Mnqobi <msibiya05@ai.wethinkcode.co.za>
2025-11-24 11:27:39 +0200

    final chatbot without logic

--- src/Components/ChatInput.jsx
+++ src/Components/ChatInput.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { ArrowUp } from 'lucide-react';
 
 const ChatInput = ({ onSend }) => {
 
@@ -14,11 +15,17 @@ const ChatInput = ({ onSend }) => {
   setInput("")
 }
 
+ function handleKeyDown(e){
+  if(e.key === 'Enter'){
+    handleSend();
+  }
+ }
+
 
   return (
-    <div className='flex justify-center items-center gap-2 p-4'>
-        <input type="text" placeholder="Send message to Chatbot" value={input} onChange={handleChange} size={30} className='rounded-[20px] outline-none border border-gray-400 px-4 py-2'/>
-        <button className='bg-blue-500 px-5 py-2 rounded-[10px] cursor-pointer' onClick={handleSend}>Send</button>
+    <div className='flex justify-between items-center gap-2  w-100% border border-gray-300 px-4 mx-2 mb-2 py-2 bg-white rounded-full mt-10'>
+        <input type="text" placeholder="Send message to Chatbot" value={input} onChange={handleChange} onKeyDown={handleKeyDown} size={30} className='rounded-[20px] outline-none px-4 py-2'/>
+        <button className='bg-blue-500 px-1 py-1 rounded-full cursor-pointer text-white' onClick={handleSend}><ArrowUp /></button>
     </div>
   )
 }
--- src/Components/ChatMessage.jsx
+++ src/Components/ChatMessage.jsx
@@ -4,8 +4,8 @@ const ChatMessage = ({message, sender}) => {
 
 
   return (
-    <div className={`flex items-center gap-2 p-4 ${sender === "user" ? 'justify-end' : 'justify-start '}`}>
-        {sender === "bot" && <img src="src/assets/ai-chatting.png" className='w-[30px]' alt="bot-image" />}
+    <div className={`flex items-center gap-2 p-4  rounded-2xl text-sm  ${sender === "user" ? 'justify-end ' : 'justify-start '}`}>
+        {sender === "bot" && <img src="src/assets/chatbot.jpg" className='w-[30px]' alt="bot-image" />}
         <p>{message}</p>
         {sender === "user" && <img src="src/assets/user-icon.webp" className='w-[30px]' alt="user-image" />}
     </div>
